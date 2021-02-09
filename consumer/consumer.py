c58513cfb91560126c07e90c815ce71114078958
Major Hayden <major@redhat.com>
2021-02-09 16:22:09 -0600

    Handle the new style of receiving messages

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -16,12 +16,17 @@ sqs = boto3.client('sqs', region_name=SQS_REGION)
 print("Webhook consumer starting up!")
 
 while True:
-    for message in sqs.receive_message(
-            QueueUrl=SQS_QUEUE_URL,
-            WaitTimeSeconds=20,
-            MaxNumberOfMessages=10)["Messages"]:
-        
-        print(f"Got message: {message} at {datetime.datetime.now().isoformat()}")
+    message_list = sqs.receive_message(
+        QueueUrl=SQS_QUEUE_URL,
+        WaitTimeSeconds=20,
+        MaxNumberOfMessages=10
+    )
+    if not message_list or "Messages" not in message_list:
+        time.sleep(5)
+        continue
+
+    for message in message_list["Messages"]:
+        print(f"Got message: {message['MessageId']} at {datetime.datetime.now().isoformat()}")
         parsed = json.loads(message["Body"])
         original_headers = parsed['headers']
         payload = parsed['payload']
